89fe230d1518b0dbbd6ba211aa831db571f80f44
Cristian Zurita <czurita.westnet@gmail.com>
2022-11-20 13:37:34 -0300

    rol, user controllers were fixed

--- app/Http/Controllers/RolController.php
+++ app/Http/Controllers/RolController.php
@@ -9,26 +9,27 @@ use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
 use Illuminate\Support\Facades\DB;
 
+
 class RolController extends Controller
 {
-
     function __construct()
     {
-        $this->middleware('permission: ver-rol | crear-rol | editar-rol | borrar-rol' , ['only' => ['index']]);  
-        $this->middleware('permission: crear-rol' , ['only' => ['create', 'store']]);   
-        $this->middleware('permission: editar-rol' , ['only' => ['edit', 'update']]);   
-        $this->middleware('permission: borrar-rol' , ['only' => ['destroy']]);   
+        $this->middleware('permission:ver-rol|crear-rol|editar-rol|borrar-rol', ['only' => ['index']]);
+        $this->middleware('permission:crear-rol', ['only' => ['create','store']]);
+        $this->middleware('permission:editar-rol', ['only' => ['edit','update']]);
+        $this->middleware('permission:borrar-rol', ['only' => ['destroy']]);
     }
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
+    public function index(Request $request)
+    {        
+         //Con paginación
         $roles = Role::paginate(5);
-        return view('roles.index', compact('roles'));
-
+        return view('roles.index',compact('roles'));
+         //al usar esta paginacion, recordar poner en el el index.blade.php este codigo  {!! $roles->links() !!} 
     }
 
     /**
@@ -39,7 +40,7 @@ class RolController extends Controller
     public function create()
     {
         $permission = Permission::get();
-        return view ('roles.crear', compact('permission'));
+        return view('roles.crear',compact('permission'));
     }
 
     /**
@@ -50,11 +51,15 @@ class RolController extends Controller
      */
     public function store(Request $request)
     {
-        $this->validate($request, ['name' => 'required', 'permission' => 'required']);
-        Role::create(['name' => $request->input('name')]);
+        $this->validate($request, [
+            'name' => 'required|unique:roles,name',
+            'permission' => 'required',
+        ]);
+    
+        $role = Role::create(['name' => $request->input('name')]);
         $role->syncPermissions($request->input('permission'));
-
-        return redirect()->route('roles.index');
+    
+        return redirect()->route('roles.index');                        
     }
 
     /**
@@ -78,11 +83,11 @@ class RolController extends Controller
     {
         $role = Role::find($id);
         $permission = Permission::get();
-        $rolePermissions = DB::table('role_has_permissions')->where('role_has_permissions.role_id', $id)
-            ->pluck('role_has_permissions.permission_id', 'role_has_permissions.permission_id')
+        $rolePermissions = DB::table("role_has_permissions")->where("role_has_permissions.role_id",$id)
+            ->pluck('role_has_permissions.permission_id','role_has_permissions.permission_id')
             ->all();
-
-        return view('roles.editar', compact('role', 'permission', 'rolPermissions'));
+    
+        return view('roles.editar',compact('role','permission','rolePermissions'));
     }
 
     /**
@@ -94,14 +99,18 @@ class RolController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $this->validate($request, ['name' => 'required', 'permission' => 'required']); 
-        
+        $this->validate($request, [
+            'name' => 'required',
+            'permission' => 'required',
+        ]);
+    
         $role = Role::find($id);
         $role->name = $request->input('name');
         $role->save();
-        $role->syncPermissions ($request->input('permission'));
-
-        return redirect()->route('roles.index');
+    
+        $role->syncPermissions($request->input('permission'));
+    
+        return redirect()->route('roles.index');                        
     }
 
     /**
@@ -112,8 +121,7 @@ class RolController extends Controller
      */
     public function destroy($id)
     {
-        DB::table('roles')->where('id', $id)->delete();
-        
-        return redirect()->route('roles.index');
+        DB::table("roles")->where('id',$id)->delete();
+        return redirect()->route('roles.index');                        
     }
 }
