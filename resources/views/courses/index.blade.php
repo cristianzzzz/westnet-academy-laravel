b3e732d88df0fa11844c21034cd8253dd17e3fb0
Cristian Zurita <czurita.westnet@gmail.com>
2022-11-20 17:01:32 -0300

    course views added

---
+++ resources/views/courses/index.blade.php
@@ -0,0 +1,60 @@
+@extends('layouts.app')
+
+@section('content')
+    <section class="section">
+        <div class="section-header">
+            <h3 class="page__heading">Cursos</h3>
+        </div>
+        <div class="section-body">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-body">
+                
+            
+                        @can('crear-curso')
+                        <a class="btn btn-warning" href="{{ route('courses.create') }}">Nuevo</a>
+                        @endcan
+            
+                        <table class="table table-striped mt-2">
+                                <thead style="background-color:#6777ef">                                     
+                                    <th style="display: none;">ID</th>
+                                    <th style="color:#fff;">Titulo</th>
+                                    <th style="color:#fff;">Contenido</th>                                    
+                                    <th style="color:#fff;">Acciones</th>                                                                   
+                                </thead>
+                            <tbody>
+                            @foreach ($courses as $course)
+                            <tr>
+                                <td style="display: none;">{{ $course->id }}</td>                                
+                                <td>{{ $course->titulo }}</td>
+                                <td>{{ $course->contenido }}</td>
+                                <td>
+                                    <form action="{{ route('courses.destroy',$course->id) }}" method="POST">                                        
+                                        @can('editar-curso')
+                                        <a class="btn btn-info" href="{{ route('courses.edit',$course->id) }}">Editar</a>
+                                        @endcan
+
+                                        @csrf
+                                        @method('DELETE')
+                                        @can('borrar-curso')
+                                        <button type="submit" class="btn btn-danger">Borrar</button>
+                                        @endcan
+                                    </form>
+                                </td>
+                            </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+
+                        <!-- Ubicamos la paginacion a la derecha -->
+                        <div class="pagination justify-content-end">
+                            {!! $courses->links() !!}
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
