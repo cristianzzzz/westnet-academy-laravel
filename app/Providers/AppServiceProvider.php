8fc7043039956f6f95c6575280a5b66825a9a1dc
Cristian Zurita <czurita.westnet@gmail.com>
2022-11-19 22:59:35 -0300

    some schemas were added

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\PaginationServiceProvider;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +26,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(192);
+        Paginator::useBootstrap();
     }
 }
