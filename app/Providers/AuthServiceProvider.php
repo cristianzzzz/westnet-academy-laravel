9bbd777a0f462492db282e9752ef3ee5beffb366
Cristian Zurita <czurita.westnet@gmail.com>
2022-11-20 11:40:49 -0300

    SuperAdmin user was created

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,8 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::before(function ($user, $ability) {
+            return $user->email == 'admin@westnet.com.ar' ?? null;
+        });
     }
 }
